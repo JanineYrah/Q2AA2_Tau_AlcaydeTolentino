6f403ffebc022dfcd9f4aa42c308a195f8f37736
JanineYrah <b2027jytolentino@pshs.edu.ph>
2024-12-18 22:17:53 +0800

    Update SkeletonCodeForAA.java

--- SkeletonCodeForAA/src/skeletoncodeforaa/SkeletonCodeForAA.java
+++ SkeletonCodeForAA/src/skeletoncodeforaa/SkeletonCodeForAA.java
@@ -11,27 +11,13 @@ public class SkeletonCodeForAA {
         //Use Case #1: Attack enemy with weapon
         System.out.println("Use Case #1: Attack enemy with Weapon");
         System.out.println("--------------------------------------");
-        Character player = new player();
-        Enemy enemy1 = new Enemy();
-        enemy1.spawn(Surrounding);
-
-        if(player.getLocation() in enemy.getSurrounding()) {
-            enemy1.setProvoked(true);
-            enemy.attack(player);
-        }
-
-        if (Item n instanceOf Weapon) {
-            Player.attack(enemy1);
-        } else {
-            System.out.println("Cannot attack with the equipped Item");
-        }
 
 
     
         //Use Case #2: Picking up an Item
         System.out.println("Use Case #2: Picking up an Item");
         System.out.println("--------------------------------------");
-
+        
 
 
 
